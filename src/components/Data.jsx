e175e02eb59cd7d08497bc3828b7460e16cedb66
HPPro <alexisgebrah@gmail.com>
2024-12-08 00:49:50 +0200

    React - redux

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,38 +1,37 @@
 import Form from "./Form.jsx";
 import Weather from "./Weather.jsx";
 import {api_key, base_url} from "../utils/constants.js";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {changeMessage} from "../actions/messageAction.js";
 import {changeWeather} from "../actions/weatherAction.js";
 
 
 const Data = () => {
+    const {city} = useSelector(state => state.cityState)
     const dispatch = useDispatch();
 
-    const getWeather = city => {
-        fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
-            .then(result => result.json())
-            .then(data => {
-                dispatch(changeWeather({
-                    country: data.sys.country,
-                    city: data.name,
-                    temp: data.main.temp,
-                    pressure: data.main.pressure,
-                    sunset: new Date(data.sys.sunset * 1000)
-                }))
-                const message = '';
-                dispatch(changeMessage(message))
-            })
-            .catch(e => {
-                console.log(e);
-                const message = 'Enter correct city name';
-                dispatch(changeMessage(message))
-            })
-    }
+    fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
+        .then(result => result.json())
+        .then(data => {
+            dispatch(changeWeather({
+                country: data.sys.country,
+                city: data.name,
+                temp: data.main.temp,
+                pressure: data.main.pressure,
+                sunset: new Date(data.sys.sunset * 1000)
+            }))
+            const message = '';
+            dispatch(changeMessage(message))
+        })
+        .catch(e => {
+            console.log(e);
+            const message = 'Enter correct city name';
+            dispatch(changeMessage(message))
+        })
 
     return (
         <div>
-            <Form />
+            <Form/>
             <Weather/>
         </div>
     );
